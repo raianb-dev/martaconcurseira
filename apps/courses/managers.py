ae6aee5f827129f7fc1b28c7104ccb26bf127304
eticialima <leticialimacgi@gmail.com>
2023-01-11 17:01:12 -0300

    volta config

--- apps/courses/managers.py
+++ apps/courses/managers.py
@@ -5,7 +5,7 @@ from django.db import models
 class CourseManager(models.Manager):
 
     def search(self, query=None):
-        qs = self.get_queryset().filter(is_active=True)
+        qs = self.get_queryset()
         if query is not None:
             or_lookup = (Q(name__icontains=query) | Q(short_description__icontains=query) |
                          Q(keywords__icontains=query) | Q(author__icontains=query) | Q(description__icontains=query))
